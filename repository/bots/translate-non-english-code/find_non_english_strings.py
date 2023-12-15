40819a4b1e71b9a33dcd191af56801f2f9cc7652
Shailendra Sharma <shailendra.sharma@gmail.com>
2023-12-16 00:59:15 +0530

    added matching for many more languages

--- repository/bots/translate-non-english-code/find_non_english_strings.py
+++ repository/bots/translate-non-english-code/find_non_english_strings.py
@@ -7,41 +7,137 @@ import argparse
 EXTENSIONS = ['.sql']
 
 # Regex for non-English characters (outside Basic Latin and Latin-1 Supplement)
-# NON_ENGLISH_PATTERN = r'[^\u0000-\u007F]+'
 ENGLISH_OR_TECHNICAL_PATTERN = r'^[A-Za-z0-9\s,.?!;:\'\"(){}[\]/\\@#$%^&*+=<>_`~|-]+$'
 
-# Regex patterns for SQL comments and string literals
-COMMENT_PATTERN = r'--.*?$|/\*.*?\*/'
-STRING_LITERAL_PATTERN = r"'([^']*)'"
+# Supported file extensions and their comment/string patterns
+LANG_PATTERNS = {
+    '.sql': {
+        'single_line_comment': [r'--.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r"'([^']*)'"],
+        'multi_line_string': []
+    },
+    '.json': {
+        'single_line_comment': [],
+        'multi_line_comment': [],
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': []
+    },
+    '.yml': {
+        'single_line_comment': [r'#.*'],
+        'multi_line_comment': [],
+        'single_line_string': [r"'([^']*)'", r'"([^"]*)"'],
+        'multi_line_string': []
+    },
+    '.ts': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r"'([^']*)'", r'"([^"]*)"', r'`([^`]*)`'],
+        'multi_line_string': []
+    },
+    '.tsx': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r"'([^']*)'", r'"([^"]*)"', r'`([^`]*)`'],
+        'multi_line_string': []
+    },
+    '.js': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r"'([^']*)'", r'"([^"]*)"', r'`([^`]*)`'],
+        'multi_line_string': []
+    },
+    '.md': {
+        'single_line_comment': [],
+        'multi_line_comment': [r'<!--.*?-->'],
+        'single_line_string': [],
+        'multi_line_string': []
+    },
+    '.css': {
+        'single_line_comment': [],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [],
+        'multi_line_string': []
+    },
+    '.go': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': [r'`([^`]*)`']  # Raw string literals in Go
+    },
+    '.java': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': []
+    },
+    '.rs': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*[^*]*\*+(?:[^/*][^*]*\*+)*/'],  # Nested comments support
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': [r'r#*"([^"]*)"#']  # Raw string literals in Rust
+    },
+    '.c': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': []
+    },
+    '.cpp': {
+        'single_line_comment': [r'//.*?$'],
+        'multi_line_comment': [r'/\*.*?\*/'],
+        'single_line_string': [r'"([^"]*)"'],
+        'multi_line_string': []
+    },
+    '.py': {
+        'single_line_comment': [r'#.*'],
+        'multi_line_comment': [],  # Python uses multi-line strings as multi-line comments
+        'single_line_string': [r'"([^"]*)"', r"'([^']*)'"],
+        'multi_line_string': [r'"""(.*?)"""', r"'''(.*?)'''"]
+    }
+}
 
 
 def is_english_or_technical(text):
     return re.match(ENGLISH_OR_TECHNICAL_PATTERN, text) is not None
 
 
-def find_non_english_in_line(line):
+PATTERN_CHECK_SEQUENCE = ['multi_line_comment', 'multi_line_string', 'single_line_comment', 'single_line_string']
+
+
+def find_non_english_in_line(line, patterns):
     results = []
 
-    # Check for comments
-    comments = re.findall(COMMENT_PATTERN, line, re.DOTALL)
-    for comment in comments:
-        if comment.strip() and not is_english_or_technical(comment):
-            results.append({'type': 'comment', 'text': comment})
+    for check_sequence in PATTERN_CHECK_SEQUENCE:
+        for pattern in patterns.get(check_sequence, []):
+            matches = re.findall(pattern, line, re.DOTALL)
+            for match in matches:
+                if match.strip() and not is_english_or_technical(match):
+                    results.append({'type': check_sequence, 'text': match})
+                    break
 
-    # Check for string literals
-    string_literals = re.findall(STRING_LITERAL_PATTERN, line)
-    for string_literal in string_literals:
-        if string_literal.strip() and not is_english_or_technical(string_literal):
-            results.append({'type': 'literal', 'text': string_literal})
+    # # Check for multi-line comments or strings
+    # for pattern in patterns.get('multi_line_comment', []) + patterns.get('multi_line_string', []):
+    #     matches = re.findall(pattern, line, re.DOTALL)
+    #     for match in matches:
+    #         if match.strip() and not is_english_or_technical(match):
+    #             results.append({'type': 'multi-line', 'text': match})
+    #
+    # # Check for single-line comments or strings
+    # for pattern in patterns.get('single_line_comment', []) + patterns.get('single_line_string', []):
+    #     matches = re.findall(pattern, line)
+    #     for match in matches:
+    #         if match.strip() and not is_english_or_technical(match):
+    #             results.append({'type': 'single-line', 'text': match})
 
     return results
 
 
-def process_file(file_path):
+def process_file(file_path, patterns):
     non_english_map = []
     with open(file_path, 'r', encoding='utf-8') as file:
         for line_number, line in enumerate(file, 1):
-            non_english_items = find_non_english_in_line(line)
+            non_english_items = find_non_english_in_line(line, patterns)
             if non_english_items:
                 non_english_map.append({'line': line_number, 'items': non_english_items})
     return non_english_map
@@ -52,9 +148,11 @@ def main(input_path, output_file):
 
     for root, dirs, files in os.walk(input_path):
         for file in files:
-            if any(file.endswith(ext) for ext in EXTENSIONS):
+            file_ext = os.path.splitext(file)[1]
+            if file_ext in LANG_PATTERNS:
                 file_path = os.path.join(root, file)
-                file_map = process_file(file_path)
+                patterns = LANG_PATTERNS[file_ext]
+                file_map = process_file(file_path, patterns)
                 if file_map:
                     project_map.append({'file': file_path, 'map': file_map})
 
@@ -64,7 +162,7 @@ def main(input_path, output_file):
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Process SQL files for non-English text.')
+    parser = argparse.ArgumentParser(description='Process files for non-English text.')
     parser.add_argument('input_path', type=str, help='Input file path')
     parser.add_argument('output_file', type=str, help='Output file name')
 
